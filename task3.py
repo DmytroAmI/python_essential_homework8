f2369410f9e8e2f72fd3b7593c90aabd7f00b655
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-04-03 19:41:52 +0300

    update task3

--- task3.py
+++ task3.py
@@ -1,21 +1,21 @@
 import json
 import pickle
 
-data = ["item1, item2, item3", "item4, item5"]
+data = ["предмет1, item2, item3", "item4, item5"]
 
 # pickle
-with open("data.txt", "wb") as file:
+with open("data.pickle", "wb") as file:
     pickle.dump(data, file)
 
-with open("data.txt", "rb") as file:
+with open("data.pickle", "rb") as file:
     data_pickle = pickle.load(file)
 print(data_pickle)
 
 # json
 data_json = json.loads(json.dumps(data_pickle))
-with open("output.json", "w") as file:
-    json.dump(data_json, file)
+with open("output.json", "w", encoding="utf-8") as file:
+    json.dump(data_json, file, ensure_ascii=False, indent=4)
 
-with open("output.json", "r") as file:
+with open("output.json", "r", encoding="utf-8") as file:
     data_j = json.load(file)
 print(data_j)
